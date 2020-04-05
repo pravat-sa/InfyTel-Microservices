5d0e40bb610076782d003358782d727bcf4fc926
pravat-sa <bindas.pksahoo@gmail.com>
2020-04-05 13:03:15 +0530

    First commit

---
+++ InfyTel-config-server/src/test/java/com/infosys/infytel/configserver/InfyTelConfigServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.infosys.infytel.configserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyTelConfigServerApplicationTests {
+
+	/*
+	 * @Test void contextLoads() { }
+	 */
+
+}
