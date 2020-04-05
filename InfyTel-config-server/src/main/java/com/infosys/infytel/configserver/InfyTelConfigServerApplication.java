286623d4c30944db1f9d70d8a7b1b4c22c1a1cc8
pravat-sa <bindas.pksahoo@gmail.com>
2020-04-05 13:29:08 +0530

    changes 1.0

--- InfyTel-config-server/src/main/java/com/infosys/infytel/configserver/InfyTelConfigServerApplication.java
+++ InfyTel-config-server/src/main/java/com/infosys/infytel/configserver/InfyTelConfigServerApplication.java
@@ -2,12 +2,13 @@ package com.infosys.infytel.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class InfyTelConfigServerApplication {
+@EnableConfigServer
+public class InfytelConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InfyTelConfigServerApplication.class, args);
+		SpringApplication.run(InfytelConfigServerApplication.class, args);
 	}
-
 }
